3c285071232dd65796a2f8eeaf41cf49cf652615
Jamil-coder360 <jamil595nhj@gmail.com>
2026-04-27 18:15:35 +0600

    redux add

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,17 @@ import "swiper/css";
 import App from "./App";
 import AuthProvider from "./context/AuthContext";
 import ThemeProvider from "./context/ThemeContext";
+import store from "./store";
+import { Provider } from "react-redux";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <ThemeProvider>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </ThemeProvider>
+    <Provider store={store}>
+      <ThemeProvider>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </ThemeProvider>
+    </Provider>
   </StrictMode>,
 );
